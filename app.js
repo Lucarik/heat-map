aeda67cdb050b83a36029810aa1644eb68b391cb
Justin Sterling <justinsterling44@gmail.com>
2023-11-13 14:34:23 -0500

    Added more color variety, changes to style

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const  margin = {top: 50, right: 30, bottom: 75, left: 70};
+const  margin = {top: 50, right: 30, bottom: 80, left: 70};
 const w = 1000 - margin.left - margin.right;
-const h = 500 - margin.top - margin.bottom;
+const h = 550 - margin.top - margin.bottom;
 
 // Initialize svg, set width, height 
 const svg = d3.select(".plot")
@@ -26,16 +26,16 @@ async function getData() {
     let title = "Monthly Global Land-Surface Temperature";
     let yAxisTitle = "Months";
     let xAxisTitle = "Years";
-    //console.log(dataset.monthlyVariance)
     
-    const datasetCombined = dataset.monthlyVariance.map(data => [data.year,data.month,data.variance, new Date(data.year,data.month,1)]);
+    const datasetCombined = dataset.monthlyVariance.map(data => [data.year,data.month-1,data.variance, new Date(data.year,data.month,1)]);
 
     const baseTemp = dataset.baseTemperature;
     
-    let barw = w / dataset.length;
-
-    let colorArray = ["red","green", "lightblue", "white", "orange", "gray"];
-    //console.log(d3.min(datasetCombined, (d) => new Date(d[0],0)));
+    // Arrays for determining heatmap color
+    let tempNumArray = [2.8,3.6,4.3,5.1,5.9,6.7,7.4,8.2,9,9.7,10.5,11.3,12,12.8];
+    let colorArray = ["rgb(50,50,250)", "rgb(70,70,250)", "rgb(100,100,250)", "rgb(130,130,250)", "rgb(160,160,250)", "rgb(195,195,250)", "rgb(230,230,250)"
+    ,"rgb(250,250,250", "rgb(250,230,230)", "rgb(250,195,195)", "rgb(250,160,160)", "rgb(250,130,130)", "rgb(250,100,100)", "rgb(250,70,70)"];
+    
     // Tooltip
     const tooltip = d3.select(".plot")
         .append("g")
@@ -52,29 +52,29 @@ async function getData() {
     // Integer(1-12) to month
     const getMonth = function(data) {
         switch(data) {
-            case 1:
+            case 0:
                 return "January";
-            case 2:
+            case 1:
                 return "February";
-            case 3:
+            case 2:
                 return "March";
-            case 4:
+            case 3:
                 return "April";
-            case 5:
+            case 4:
                 return "May";
-            case 6:
+            case 5:
                 return "June";
-            case 7:
+            case 6:
                 return "July";
-            case 8:
+            case 7:
                 return "August";
-            case 9:
+            case 8:
                 return "September";
-            case 10:
+            case 9:
                 return "October";
-            case 11:
+            case 10:
                 return "November";
-            case 12:
+            case 11:
                 return "December";
             default:
                 return '';
@@ -84,12 +84,20 @@ async function getData() {
     // Set color for data points based on data present
     const initializeColor = function(data) {
         let temp = baseTemp + data[2];
-        if (temp < 5) return colorArray[1];
-        else if (temp < 7.2) return colorArray[2];
-        else if (temp < 8.3) return colorArray[3];
-        else if (temp < 10.6) return colorArray[4];
-        else if (temp < 12.8) return colorArray[5];
-        else return "black"
+        if (temp < tempNumArray[0]) return colorArray[0];
+        else if (temp < tempNumArray[1]) return colorArray[1];
+        else if (temp < tempNumArray[2]) return colorArray[2];
+        else if (temp < tempNumArray[3]) return colorArray[3];
+        else if (temp < tempNumArray[4]) return colorArray[4];
+        else if (temp < tempNumArray[5]) return colorArray[5];
+        else if (temp < tempNumArray[6]) return colorArray[6];
+        else if (temp < tempNumArray[7]) return colorArray[7];
+        else if (temp < tempNumArray[8]) return colorArray[8];
+        else if (temp < tempNumArray[9]) return colorArray[9];
+        else if (temp < tempNumArray[10]) return colorArray[10];
+        else if (temp < tempNumArray[11]) return colorArray[11];
+        else if (temp < tempNumArray[12]) return colorArray[12];
+        else return "rgb(250,50,50)"
     }
 
     // Function called when moving mouse out of bar 
@@ -111,14 +119,14 @@ async function getData() {
         tooltip.attr("data-month", data[1]);
         let temp = (baseTemp + data[2]).toFixed(2);
         const text = d3.select('.tooltip-text');
-        text.html(`Date: ${data[1]}, ${data[0]}<br/>Temperature: ${temp}℃
+        text.html(`Date: ${getMonth(data[1])}, ${data[0]}<br/>Temperature: ${temp}℃
         <br/>Variance: ${data[2]}℃`);
         const [x, y] = d3.mouse(this);
         tooltip.style("left", `${x+90}px`)
             .style("top", `${y-510}px`)
     };
 
-    // Append title and y axis title
+    // Append title and x/y axis title
     svg.append('text')
         .attr("class", "text")
         .attr('transform', 'rotate(-90)')
@@ -128,8 +136,8 @@ async function getData() {
         .text(yAxisTitle);
         svg.append('text')
         .attr("class", "text")
-        .attr('x', 450)
-        .attr('y', 435)
+        .attr('x', 490)
+        .attr('y', 475)
         .style("font-size", "18px")
         .text(xAxisTitle);
     svg.append('text')
@@ -140,9 +148,6 @@ async function getData() {
         .attr('y', -10)
         .text(title);
 
-    let minDate = d3.min(datasetCombined, (d) => d[0]);
-    let maxDate = d3.max(datasetCombined, (d) => d[0]);
-    //console.log(minDate);
     const YearFormat = d3.utcFormat('%Y');
     const MonthFormat = d3.utcFormat('%B');
     // Create x scale, append x axis
@@ -167,10 +172,9 @@ async function getData() {
             .style("text-anchor", "end")
             .style("fill", "white");
 
-    //console.log(maxMonth.getMonth());
     // Create y scale, append y axis
     const y = d3.scaleBand()
-    .domain([1,2,3,4,5,6,7,8,9,10,11,12])
+    .domain([0,1,2,3,4,5,6,7,8,9,10,11])
     .range([h, 0]);
         
     svg.append("g")
@@ -192,9 +196,10 @@ async function getData() {
             .attr("y", d => y(d[1]))
             .attr('width', d => x.bandwidth(d[0]))
             .attr('height', d => y.bandwidth(d[1]))
-            .attr("data-xvalue", d => d[0])
-            .attr("data-yvalue", d => d[1])
-            .attr("class", "bar")
+            .attr("data-year", d => d[0])
+            .attr("data-month", d => d[1])
+            .attr("data-temp", d => d[2])
+            .attr("class", "cell")
             .attr("fill", initializeColor)
             .on("mouseover", mouseover)
             .on("mouseout", mouseout)
@@ -202,24 +207,26 @@ async function getData() {
 
     // Create and append legend
     var threshold = d3.scaleThreshold()
-        .domain([2.8,5.0,7.2,8.3,10.6,12.8])
+        .domain(tempNumArray)
         .range(colorArray);
 
     var legendScale = d3.scaleLinear()
         .domain([2.8, 12.8])
-        .range([0, 240]);
+        .range([0, 350]);
     var legendAxis = d3.axisBottom(legendScale)
         .tickSize(13)
         .tickValues(threshold.domain())
         .tickFormat(d => d.toFixed(1));
         
     var g = svg.append("g");
-
+    // Data text at bottom of legend
     g.call(legendAxis)
-        .attr("transform", "translate(10,445)")
+        .attr("transform", "translate(10,475)")
+        .attr("id", "legend")
         .selectAll("text")
             .style("fill", "white");;
-    console.log(threshold.invertExtent("blue"));
+    
+    // Colors in legend
     g.selectAll("rect")
         .data(threshold.range().map(function(color) {
             var d = threshold.invertExtent(color);
@@ -234,6 +241,7 @@ async function getData() {
             .attr("width", function(d) { return legendScale(d[1]) - legendScale(d[0]); })
             .attr("fill", function(d) { return threshold(d[0]); });
 
+    // Legend description
     g.append("text")
         .attr("fill", "#000")
         .attr("font-weight", "bold")
